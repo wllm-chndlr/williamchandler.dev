f05c94a606018f1fd6f08208491c5c5242462569
William Chandler <chndlr.wllm@gmail.com>
2019-03-13 08:50:40 -0500

    project card, images directory

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -4,10 +4,14 @@ export default Route.extend({
     model() {
         return [
             {
-            title: 'Top Taco ATX',
-            category: 'Estate',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-            description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+                title: 'Top Taco ATX',
+                image: 'assets/images/toptaco.png',
+                description: 'Real-time ratings aggregator for ATX taco joints',
+            }, 
+            {
+                title: 'Simpsons Clicky Game',
+                image: 'assets/images/simpsons.png',
+                description: 'Memory game built with React',
             }, 
         ];
     }
