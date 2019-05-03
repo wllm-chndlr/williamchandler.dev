2042e5bb589502706dce07a2f2972ead944f6b6a
William Chandler <chndlr.wllm@gmail.com>
2019-05-02 21:00:55 -0500

    animated-each implementation

---
+++ app/components/animated-each.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import move from 'ember-animated/motions/move';
+import { fadeOut } from 'ember-animated/motions/opacity';
+
+export default Component.extend({
+  init() {
+    this._super();
+    this.items = ['A', 'B', 'C', 'D', 'E'];
+  },
+
+  * transition({ keptSprites, removedSprites }) {
+    for (let sprite of keptSprites) {
+      move(sprite);
+    }
+
+    for (let sprite of removedSprites) {
+      fadeOut(sprite);
+    }
+  },
+
+  removeItem(item){
+    this.set('items', this.items.filter(i => i !== item));
+  }
+});
--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        const tech = [
+            'Ember.js',
+            'Node.js',
+            'HTML5',
+            'CSS3',
+            'Sass',
+            'CSS Grid',
+            'React.js',
+            'D3',
+            'Express',
+            'MongoDB',
+        ]
+
+        return tech;
+    }
 });
