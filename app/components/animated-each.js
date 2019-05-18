622e9c6ea7bb493aad4346567eda094560808a53
William Chandler <chndlr.wllm@gmail.com>
2019-05-18 17:26:46 -0500

    removed problematic animated-each, layout updates

--- app/components/animated-each.js
+++ app/components/animated-each.js
@@ -1,24 +1,24 @@
-import Component from '@ember/component';
-import move from 'ember-animated/motions/move';
-import { fadeOut } from 'ember-animated/motions/opacity';
+// import Component from '@ember/component';
+// import move from 'ember-animated/motions/move';
+// import { fadeOut } from 'ember-animated/motions/opacity';
 
-export default Component.extend({
-  init() {
-    this._super();
-    this.items = ['A', 'B', 'C', 'D', 'E'];
-  },
+// export default Component.extend({
+//   init() {
+//     this._super();
+//     this.items = ['A', 'B', 'C', 'D', 'E'];
+//   },
 
-  * transition({ keptSprites, removedSprites }) {
-    for (let sprite of keptSprites) {
-      move(sprite);
-    }
+//   * transition({ keptSprites, removedSprites }) {
+//     for (let sprite of keptSprites) {
+//       move(sprite);
+//     }
 
-    for (let sprite of removedSprites) {
-      fadeOut(sprite);
-    }
-  },
+//     for (let sprite of removedSprites) {
+//       fadeOut(sprite);
+//     }
+//   },
 
-  removeItem(item){
-    this.set('items', this.items.filter(i => i !== item));
-  }
-});
+//   removeItem(item){
+//     this.set('items', this.items.filter(i => i !== item));
+//   }
+// });
