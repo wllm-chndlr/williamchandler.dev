dc37a2e88a2fb779788bb877f9566b6f17c669c3
William Chandler <chndlr.wllm@gmail.com>
2019-08-19 21:06:53 -0500

    new project links

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -9,28 +9,35 @@ export default Route.extend({
                 description: 'Real-time ratings aggregator for ATX taco joints',
                 url: 'https://wllm-chndlr.github.io/TopTaco/',
                 github: 'https://github.com/wllm-chndlr/TopTaco'
-            }, 
+            },
             {
                 title: 'Simpsons Clicky Game',
                 image: 'assets/images/simpsons.png',
                 description: 'Memory game built with React',
-                // url: 
-                // github: 
-            }, 
+                url: 'https://wllm-chndlr.github.io/clicky-game/',
+                github: 'https://github.com/wllm-chndlr/clicky-game'
+            },
             {
                 title: 'Rad Drumkit',
                 image: 'assets/images/drumkit.png',
                 description: 'A pretty rad keyboard-based drumkit',
-                // url: 
-                // github: 
-            }, 
+                url: 'https://wllm-chndlr.github.io/drumkit/',
+                github: 'https://github.com/wllm-chndlr/drumkit'
+            },
+            {
+                title: 'Guac-a-mole',
+                image: 'assets/images/guac.png',
+                description: 'Tex-Mex twist on the classic Whack-a-Mole game',
+                url: 'https://wllm-chndlr.github.io/guac-a-mole/',
+                github: 'https://github.com/wllm-chndlr/guac-a-mole'
+            },
             {
                 title: 'Birth Works ATX',
                 image: 'assets/images/jsc.png',
                 description: 'Site for Austin-based birth doula and educator',
-                // url: 
-                // github: 
-            }, 
+                url: 'https://www.birthworksatx.com/',
+                github: 'https://github.com/wllm-chndlr/jsc'
+            },
         ];
     }
 });
