c38c40d07cc4f0099e199bd903924705d34f8661
William Chandler <chndlr.wllm@gmail.com>
2019-09-08 16:51:01 -0500

    updated responsiveness, about buttons

--- app/routes/about.js
+++ app/routes/about.js
@@ -3,19 +3,56 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
     model() {
-        const tech = [
-            'Ember.js',
-            'Node.js',
-            'HTML5',
-            'CSS3',
-            'Sass',
-            'CSS Grid',
-            'React.js',
-            'D3',
-            'Express',
-            'MongoDB',
+        return [
+            {
+                id: 1,
+                title: 'Tech',
+                items: [
+                    'Ember.js',
+                    'Node.js',
+                    'HTML5',
+                    'CSS3',
+                    'Sass',
+                    'CSS Grid',
+                    'React.js',
+                    'D3',
+                    'Express',
+                    'MongoDB',
+                ],
+            },
+            {
+                id: 2,
+                title: 'Presentations',
+                items: [
+                    {
+                        title: 'Ember Octane: Fueling the Future',
+                        url: 'https://youtu.be/y3B0s6SHTEM',
+                        event: 'EmberATX Meetup',
+                        date: 'March 28, 2019',
+                    }
+                ],
+            },
+            {
+                id: 3,
+                title: 'Events',
+                items: [
+                    'EmberConf 2019 (attendee)',
+                    'ATX Hack for Change 2018 (participant)',
+                    'EmberATX meetup (ongoing)',
+                ],
+            },
+            {
+                id: 4,
+                title: 'Education',
+                items: [
+                    'Full Stack Web Development Bootcamp, UT Austin',
+                    'MS, International Relations, University of Glasgow',
+                    'BA, Spanish, Davidson College',
+                ],
+            },
+
         ]
 
-        return tech;
+
     }
 });
