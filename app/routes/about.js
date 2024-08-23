62567630755735f8116f6eb997f5931081c080e1
William Chandler <chndlr.wllm@gmail.com>
2024-08-22 17:12:34 -0700

    updated bio, skills

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,77 +1,71 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
-
     model() {
         return [
             {
                 id: 1,
-                title: 'Tech',
+                title: "Tech",
                 items: [
-                    'JavaScript',
-                    'HTML5',
-                    'CSS3',
-                    'Ember',
-                    'React',
-                    'Node',
-                    'Sass',
-                    'CSS Grid',
-                    'JQuery',
-                    'AJAX',
-                    'REST APIs',
-                    'D3',
-                    'SQL',
-                    'Express',
-                    'MongoDB',
+                    "JavaScript",
+                    "React",
+                    "TypeScript",
+                    "GraphQL",
+                    "Next.js",
+                    "HTML",
+                    "CSS",
+                    "Node.js",
+                    "Python",
+                    "SQL",
                 ],
             },
             {
                 id: 2,
-                title: 'Presentations',
+                title: "Presentations",
                 items: [
                     {
-                        title: 'Ember Octane: Fueling the Future',
-                        url: 'https://youtu.be/y3B0s6SHTEM',
-                        event: 'EmberATX Meetup',
-                        date: 'March 28, 2019',
-                    }
+                        title: "Ember Octane: Fueling the Future",
+                        url: "https://youtu.be/y3B0s6SHTEM",
+                        event: "EmberATX Meetup",
+                        date: "March 28, 2019",
+                    },
                 ],
             },
             {
                 id: 3,
-                title: 'Events',
+                title: "Events",
                 items: [
-                    'EmberConf 2019 (attendee)',
-                    'ATX Hack for Change 2018 (participant)',
-                    'EmberATX meetup (ongoing)',
+                    "EmberConf 2019 (attendee)",
+                    "ATX Hack for Change 2018 (participant)",
+                    "EmberATX meetup (ongoing)",
                 ],
             },
             {
                 id: 4,
-                title: 'Education',
+                title: "Education",
                 items: [
-                    'Full Stack Web Development Bootcamp, UT Austin',
-                    'MS, International Relations, University of Glasgow',
-                    'BA, Spanish, Davidson College',
+                    "Full Stack Web Development Bootcamp, UT Austin",
+                    "MS, International Relations, University of Glasgow",
+                    "BA, Spanish, Davidson College",
                 ],
             },
             {
                 id: 5,
-                title: 'Resume',
+                title: "Resume",
                 items: [
                     {
-                        blurb: 'View/download',
-                        link: 'assets/docs/william_chandler_resume.pdf'
-                    }
+                        blurb: "View/download",
+                        link: "assets/docs/william_chandler_resume.pdf",
+                    },
                 ],
             },
-        ]
+        ];
     },
 
     setupController(controller, model) {
         controller.setProperties({
             model,
-            infoToDisplay: model.get('firstObject'),
+            infoToDisplay: model.get("firstObject"),
         });
     },
 });
