c64d54a45aba997ca3db0da023927637b21e95c2
William Chandler <chndlr.wllm@gmail.com>
2025-04-08 20:21:34 -0700

    project transition, about page cleanup

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,13 +1,46 @@
 import Controller from '@ember/controller';
+import { later } from '@ember/runloop';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/template';
 
 export default Controller.extend({
+    projectToDisplay: null,
+    isLoadingProject: false,
+    projectToShow: null,
+
+    projectCardStyle: computed('isLoadingProject', function() {
+        const opacity = this.isLoadingProject ? '0' : '1';
+        const styleString = `
+            -webkit-transition: opacity 300ms ease-in-out;
+            -moz-transition: opacity 300ms ease-in-out;
+            -o-transition: opacity 300ms ease-in-out;
+            transition: opacity 300ms ease-in-out;
+            opacity: ${opacity};
+        `;
+        return htmlSafe(styleString);
+    }),
+
     actions: {
         launchProject(project) {
-            this.set('projectToDisplay', project);
+            if (project === this.projectToShow || this.isLoadingProject) {
+                 return;
+            }
+
+            this.set('isLoadingProject', true);
+
+            later(this, () => {
+                this.setProperties({
+                    projectToShow: project,
+                    isLoadingProject: false
+                });
+            }, 300);
         },
 
         resetProjects() {
-            this.set('projectToDisplay', null);
+            this.setProperties({
+                projectToDisplay: null,
+                projectToShow: (this.model && this.model.firstObject)
+            });
         },
     }
 });
